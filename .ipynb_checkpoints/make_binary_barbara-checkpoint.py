5b16f90894d751fce8179198c4ca7e1b9fd6dfb3
ulises.rey <ulises.rey@imp.ac.at>
2020-10-20 17:33:00 +0200

    commiting checkpoint files

--- .ipynb_checkpoints/make_binary_barbara-checkpoint.py
+++ .ipynb_checkpoints/make_binary_barbara-checkpoint.py
@@ -1,4 +1,4 @@
-"""read and write tiff video for binary"""
+#read and write tiff video for binary
 
 #import pckgs
 import cv2
@@ -13,21 +13,16 @@ ap.add_argument("-i", "--input_filename", required=True, help="path to input fil
 args = vars(ap.parse_args())
 
 input_filename=args['input_filename']
-bg_img_filename= args['background_filename']
-output_filename='/groups/zimmer/Ulises/wbfm/chemotaxis_assay/2020_Only_behaviour/all_good_binary/'+args['input_filename'][:-11]+'binary.tiff'
+
+output_filename='/groups/zimmer/shared_projects/Barbara_Ulises/Labeller_Test/multiple_skeleton/output_binary/'+args['input_filename'][-14:-5]+'_binary.tiff'
 
 print('\n')
 print('input:')
 print(input_filename)
 print('\n')
-print('bg:')
-print(bg_img_filename)
 print('\n')
 print('output:')
 print(output_filename)
-
-
-bg_img= tiff.imread(bg_img_filename)
 
 
 
@@ -36,20 +31,17 @@ with tiff.TiffWriter(output_filename, bigtiff=True) as tif_writer:
         for i, page in enumerate(tif.pages):
             #loads the first frame and inverts it
             img=page.asarray()
-            img=cv2.bitwise_not(img)
-            
-            #substrack background
-            img=cv2.subtract(img,bg_img)
-            
+            #img=cv2.bitwise_not(img)
+
             #median Blur
-            img[:] = cv2.medianBlur(img,5)
+            img[:] = cv2.medianBlur(img,3)
             
             #apply threshold
-            ret, new_img = cv2.threshold(img,11,255,cv2.THRESH_BINARY)
-            
+            ret, new_img = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)          
+            plt.imshow(new_img)
             #find contours
-            contours = cv2.findContours(new_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            contours = contours[0] if len(contours) == 2 else contours[1]
+            _, contours, hierarchy = cv2.findContours(new_img, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+            #contours = contours[0] if len(contours) == 2 else contours[1]
             
             #list areas of contours, find MAX, draw contours from MAX area
             areas=[]
@@ -60,6 +52,8 @@ with tiff.TiffWriter(output_filename, bigtiff=True) as tif_writer:
             img_contours = np.zeros(img.shape)
             img[:]=cv2.drawContours(img_contours,contours, worm_contour, 255, -1)
 
+            img_contours=np.array(img_contours, dtype=np.uint8)
+
             tif_writer.save(img)
-#             if i ==600: break
+            #if i ==150: break
             
