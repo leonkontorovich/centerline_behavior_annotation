3c59a0a3733ef1126b65bb72128f795a2765f373
ulises.rey <ulises.rey@imp.ac.at>
2020-12-16 15:26:17 +0100

    changes of names

--- centerline/src/make_skeleton.py
+++ centerline/src/make_skeleton.py
@@ -17,18 +17,18 @@ from skimage import data
 from skimage.util import invert
 import skimage.graph
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--input_filename", required=True, help="path to input file")
-ap.add_argument("-h5", "--h5", required=True, help="path to the DLC hdf5 file")
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "--input_filename", required=True, help="path to input file")
+# ap.add_argument("-h5", "--h5", required=True, help="path to the DLC hdf5 file")
 
-args = vars(ap.parse_args())
+# args = vars(ap.parse_args())
 
-input_filename=args['input_filename']
-print(input_filename)
-print('\n')
-h5_path=args['h5']
-print(h5_path)
-print('\n')
+# input_filename=args['input_filename']
+# print(input_filename)
+# print('\n')
+# h5_path=args['h5']
+# print(h5_path)
+# print('\n')
 
 
 
@@ -97,58 +97,58 @@ def make_skeleton(start_point, end_point, num_splines, img):
 
 
 
-df = pd.read_hdf(h5_path)
+# df = pd.read_hdf(h5_path)
 
-scorer=df.columns.get_level_values(0)[0]
-head_x=df[scorer]['Head']['x'].values
-head_y=df[scorer]['Head']['y'].values
-tail_x=df[scorer]['Tail']['x'].values
-tail_y=df[scorer]['Tail']['y'].values
+# scorer=df.columns.get_level_values(0)[0]
+# head_x=df[scorer]['Head']['x'].values
+# head_y=df[scorer]['Head']['y'].values
+# tail_x=df[scorer]['Tail']['x'].values
+# tail_y=df[scorer]['Tail']['y'].values
 
-# #create csv objects
-output_path=os.path.join('/groups/zimmer/Ulises/wbfm/chemotaxis_assay/2020_Only_behaviour/skeleton_new/',re.split('-channel',re.split('/',input_filename)[-1])[0])
+# # #create csv objects
+# output_path=os.path.join('/groups/zimmer/Ulises/wbfm/chemotaxis_assay/2020_Only_behaviour/skeleton_new/',re.split('-channel',re.split('/',input_filename)[-1])[0])
 
-print('\noutput:')
-print(output_path)
+# print('\noutput:')
+# print(output_path)
 
-csvfilePathX=open(output_path+'_skeleton_X_coords.csv','w', newline='')
-csv_writerPathX=csv.writer(csvfilePathX)
+# csvfilePathX=open(output_path+'_skeleton_X_coords.csv','w', newline='')
+# csv_writerPathX=csv.writer(csvfilePathX)
 
-csvfilePathY=open(output_path+'_skeleton_Y_coords.csv','w', newline='')
-csv_writerPathY=csv.writer(csvfilePathY)
+# csvfilePathY=open(output_path+'_skeleton_Y_coords.csv','w', newline='')
+# csv_writerPathY=csv.writer(csvfilePathY)
 
-csvfileX=open(output_path+'_spline_X_coords.csv','w', newline='')
-csv_writerX=csv.writer(csvfileX)
+# csvfileX=open(output_path+'_spline_X_coords.csv','w', newline='')
+# csv_writerX=csv.writer(csvfileX)
 
-csvfileY=open(output_path+'_spline_Y_coords.csv','w', newline='')
-csv_writerY=csv.writer(csvfileY)
+# csvfileY=open(output_path+'_spline_Y_coords.csv','w', newline='')
+# csv_writerY=csv.writer(csvfileY)
 
-csvfileK=open(output_path+'_spline_K.csv','w', newline='')
-csv_writerK=csv.writer(csvfileK)
+# csvfileK=open(output_path+'_spline_K.csv','w', newline='')
+# csv_writerK=csv.writer(csvfileK)
 
 
-num_splines=100
+# num_splines=100
 
-with tiff.TiffFile(input_filename, multifile=False) as tif:
-    for i, page in enumerate(tif.pages):
-        img=page.asarray()
+# with tiff.TiffFile(input_filename, multifile=False) as tif:
+#     for i, page in enumerate(tif.pages):
+#         img=page.asarray()
         
-        start_point=(int(head_y[i]), int(head_x[i]))
-        end_point = (int(tail_y[i]), int(tail_x[i]))
+#         start_point=(int(head_y[i]), int(head_x[i]))
+#         end_point = (int(tail_y[i]), int(tail_x[i]))
 
-        #make_skeleton_v2 function
-        u, skel_coord, spline_coord, K=make_skeleton(start_point, end_point, num_splines, img)
+#         #make_skeleton_v2 function
+#         u, skel_coord, spline_coord, K=make_skeleton(start_point, end_point, num_splines, img)
 
         
-        #csv writer
-        csv_writerPathX.writerow(skel_coord[0])
-        csv_writerPathY.writerow(skel_coord[1])
-        csv_writerX.writerow(spline_coord[0])
-        csv_writerY.writerow(spline_coord[1])
-        csv_writerK.writerow(K)
-csvfilePathX.close()
-csvfilePathY.close()
-csvfileX.close()
-csvfileY.close()
-csvfileK.close()
-print('end')
+#         #csv writer
+#         csv_writerPathX.writerow(skel_coord[0])
+#         csv_writerPathY.writerow(skel_coord[1])
+#         csv_writerX.writerow(spline_coord[0])
+#         csv_writerY.writerow(spline_coord[1])
+#         csv_writerK.writerow(K)
+# csvfilePathX.close()
+# csvfilePathY.close()
+# csvfileX.close()
+# csvfileY.close()
+# csvfileK.close()
+# print('end')
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Centerline",
+    name="centerline",
     version="0.0.1",
     author="Ulises Rey",
     author_email="ulises.rey@imp.ac.at",
