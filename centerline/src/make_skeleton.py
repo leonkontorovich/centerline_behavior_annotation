bdda2c739274c91274915161f2819cf757f42a0c
ulises.rey <ulises.rey@imp.ac.at>
2021-03-11 16:32:48 +0100

    added another function: generate_list_of_wrong_centerlines added comments for make_skeleton function. added parameter min_worm_length to make_skeleton. Now the length of the centerline has to be bigger than both min_worm_len and num_splines

--- centerline/src/make_skeleton.py
+++ centerline/src/make_skeleton.py
@@ -18,13 +18,14 @@ from skimage.util import invert
 import skimage.graph
 
 
-def make_skeleton(start_point, end_point, num_splines, img):
+def make_skeleton(start_point, end_point, min_worm_len=0, num_splines, img):
 	"""
     Make an skeleton from binary image and start and end point
     Parameters:
     -----------
 	start_point: tuple with x,y coordinates
 	end_point: tuple with x,y coordinates
+	min_worm_len: minimum worm length in pixels, if the found centerline is below it will be nan (default is 0)
 	num_splines: number of splines you want to fit
 	img: binary img from where the skeleton will be calculated
 	"""
@@ -37,7 +38,6 @@ def make_skeleton(start_point, end_point, num_splines, img):
 
 	#to increase the value a lot of the pixels outside the worm contour (np.inf will not work! sometimes head and tail outside work contour)
 	costs=np.where(costs>254.9, 255*100, costs)
-	#actual skeleton based on shortest_path of skimage
 	#actual skeleton based on route through array from skimage
 	path, cost = skimage.graph.route_through_array(costs, start=start_point, end=end_point, fully_connected=False)
 
@@ -46,7 +46,8 @@ def make_skeleton(start_point, end_point, num_splines, img):
 	x,y=np.asarray(list(zip(*path)), dtype=int)
 	#pts=np.asarray(path, dtype=np.int)
 
-	if len(pts)<num_splines:
+	#if coordinates from route_through_array are smaller than min_worm_len or num_splines, it is not a good centerline
+	if len(x)<min_worm_len or len(x)<num_splines:
 		#print('Knots are Nans in: '+str(i))
 		K=np.full(num_splines, np.nan)
 		x=np.full(num_splines, np.nan)
@@ -54,6 +55,7 @@ def make_skeleton(start_point, end_point, num_splines, img):
 		x_new=np.full(num_splines, np.nan)
 		y_new=np.full(num_splines, np.nan)
 		u=np.nan
+	#else, the path was good, fit a spline and find curvature
 	else:
 		####
         ##SHOULD THIS PART HERE BE CONVERTED TO A FUNCTION?? (or some of it)
@@ -80,3 +82,30 @@ def make_skeleton(start_point, end_point, num_splines, img):
 
 
 	return u, (x,y), (x_new, y_new), K
+
+
+def generate_list_of_wrong_centerlines(centerline_csv):
+	"""
+	Should work on the make_skeleton output or on the image (make_skeleton input?)
+	Should use the extract frames function
+
+    -----------
+	centerline: centerline csv file
+
+	"""
+	#declare wrong_centerlines empty list
+	wrong_centerlines=[]
+
+	# open file in read mode
+	with open(centerline_csv, 'r') as read_obj:
+	    # pass the file object to reader() to get the reader object
+	    csv_reader = csv.reader(read_obj)
+	    # Iterate over each row in the csv using reader object
+	    for idx, row in enumerate(csv_reader):
+	        # row variable is a list that represents a row in csv
+	        row_array=np.asarray(row, dtype=np.float64)
+	        if True in np.isnan(row_array): wrong_centerlines.append(idx)
+	        #do this to convert to array:
+
+
+	return wrong_centerlines
