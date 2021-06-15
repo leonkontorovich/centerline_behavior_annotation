018a73dbb31c318b96fbf80ed0f4630edf42ce08
Daniel Mitic <daniel.mitic@imp.ac.at>
2021-06-15 19:08:01 +0200

    functions for plotting secitons of tracks

---
+++ centerline/dev/track_plotting_daniel.py
@@ -0,0 +1,152 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+#plots all tracks for center head and tail
+def plot_tracks_center_head_tail(filename,figsize_x,figsize_y,line_width):
+    concentration_change=pd.read_csv(filename)
+    x_head_corrected=concentration_change['x_head_corrected']
+    y_head_corrected=concentration_change['y_head_corrected']
+    x_tail_corrected=concentration_change['x_tail_corrected']
+    y_tail_corrected=concentration_change['y_tail_corrected']
+    x_center_pos=concentration_change['x_center']
+    y_center_pos=concentration_change['y_center']
+    fig3, ax3 = plt.subplots(1,1, figsize = (figsize_x,figsize_y), dpi=600)
+    ax3.plot(x_head_corrected,y_head_corrected,label="head",linewidth=line_width)
+    ax3.plot(x_tail_corrected,y_tail_corrected,label="tail",linewidth=line_width)
+    ax3.plot(x_center_pos,y_center_pos,label="center",linewidth=line_width)
+    ax3.axvline(x=0, ymin=0, ymax=1, lw=10, alpha=.5, color='y')
+    ax3.plot(concentration_change['x_center'][0],concentration_change['y_center'][0], 'go', markersize=5)
+    ax3.plot(concentration_change['x_center'].tail(1),concentration_change['y_center'].tail(1), 'ro', markersize=5)
+    ax3.legend()
+    return ax3
+
+
+
+#determines the range of frames in which a specific part of the track occures
+def position_to_time(concentration_change,begin_x,end_x,begin_y,end_y):
+    """
+    determines the range of frames in which a specific part of the track occures
+    Parameters
+    -------------
+    concentration_change: dataframe with coordinate and concentration
+    begin_x,end_x,begin_y,end_y : float,integer coordinates to determine begin and end of the track section.
+    
+    """
+
+    position=concentration_change[(concentration_change['x_head_corrected'].between(begin_x,end_x))
+    & (concentration_change['y_head_corrected'].between(begin_y, end_y))]
+    return(position)
+
+
+def plot_track_section(concentration_change,position):
+    
+    """
+    plots specific part of the track
+    Parameters:
+    ---------------
+    concentration_change: dataframe with coordinate and concentration
+    position: output from position_to_time function. specific part of the track
+    
+    """
+    figsize_x=10
+    figsize_y=5
+    line_width=0.5
+    fontsize=5
+
+    #plot track
+    head_pos_x=concentration_change['x_head_corrected']
+    tail_pos_x=concentration_change['x_tail_corrected']
+    center_pos_x=concentration_change['x_center']
+
+    head_pos_y=concentration_change['y_head_corrected']
+    tail_pos_y=concentration_change['y_tail_corrected']
+    center_pos_y=concentration_change['y_center']
+
+    fig3, ax3 = plt.subplots(1,1, figsize = (figsize_x,figsize_y), dpi=600)
+    ax3.plot(head_pos_x,head_pos_y,linewidth=line_width)
+    ax3.plot(tail_pos_x,tail_pos_y,linewidth=line_width)
+    ax3.plot(center_pos_x,center_pos_y,linewidth=line_width)
+    ax3.axvline(x=0, ymin=0, ymax=1, lw=10, alpha=.5, color='y')
+    rect = patches.Rectangle((position['x_head_corrected'].min(), position['y_head_corrected'].min()),
+    abs(position['x_head_corrected'].min()-position['x_head_corrected'].max()), abs(position['y_head_corrected'].min()-position['y_head_corrected'].max()), linewidth=1, edgecolor='k', facecolor='none')
+    ax3.add_patch(rect)
+    plt.yticks(np.arange(round(min(head_pos_y)), max(head_pos_y)+1, 1.0),fontsize=fontsize)
+    plt.xticks(np.arange(round(min(head_pos_x)), max(head_pos_x)+1, 1.0),fontsize=fontsize)
+    plt.ylabel('Y (mm)')
+    plt.xlabel('X (mm)')
+    plt.show()
+    #determining part for zoom
+    fig2, ax2 = plt.subplots(1,1, figsize = (figsize_x,figsize_y), dpi=600)
+    ax2.plot(head_pos_x,head_pos_y,linewidth=line_width)
+    ax2.plot(tail_pos_x,tail_pos_y,linewidth=line_width)
+    ax2.plot(center_pos_x,center_pos_y,linewidth=line_width)
+    plt.xlim(position['x_head_corrected'].min(),position['x_head_corrected'].max())
+    plt.ylim(position['y_head_corrected'].min(),position['y_head_corrected'].max())
+    plt.ylabel('Y (mm)')
+    plt.xlabel('X (mm)')
+    ax2.axvline(x=0, ymin=0, ymax=1, lw=10, alpha=.5, color='y')
+    plt.show()
+    
+    
+    
+    
+    
+def plot_concentration_section(concentration_change,position):
+    
+    """"
+    plots concentration of speciific part of the track
+    Parameters:
+    --------------------
+    concentration_change: dataframe with coordinate and concentration
+    position: output from position_to_time function. specific part of the track
+    """
+    
+    line_width=0.5
+    x=np.arange(len(concentration_change))
+    head_conc=concentration_change['concentration_head']
+    tail_conc=concentration_change['concentration_tail']
+    center_conc=concentration_change['concentration_center']
+    xfig3, ax3 = plt.subplots(1,1, figsize = (10,5), dpi=800)
+    ax3.plot(x,head_conc,label="head",linewidth=line_width)
+    ax3.plot(x,tail_conc,label="tail",linewidth=line_width)
+    ax3.plot(x,center_conc,label="center",linewidth=line_width)
+    rect = patches.Rectangle((position.index.min(), position['concentration_head'].iloc[0]),len(position),
+    abs(position['concentration_head'].iloc[0]-position['concentration_head'].iloc[-1]), linewidth=1, edgecolor='k', facecolor='none')
+    ax3.add_patch(rect)
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    plt.show()
+
+    xfig2, ax2 = plt.subplots(1,1, figsize = (10,5), dpi=800)
+    ax2.plot(x,head_conc,label="head",linewidth=line_width)
+    ax2.plot(x,tail_conc,label="tail",linewidth=line_width)
+    ax2.plot(x,center_conc,label="center",linewidth=line_width)
+    plt.ylim(position['concentration_head'].iloc[0],position['concentration_head'].iloc[-1])
+    plt.xlim(position.index.min(),position.index.min()+len(position))
+    plt.show()
+    
+    
+       
+def curv_section(K,fps,start_frame,end_frame):
+    Ks=K[start_frame:end_frame]
+    print(('lenght of recording:'+str((end_frame-start_frame)/fps)+" secs")) #defines the part of the track i want to fourier transform
+    print('rows and columns: '+str(Ks.shape))
+    return(Ks.copy())
+
+
+def segment_averaging(Ks,win):
+    """"
+    returns the mean curvature over a defined number of segments.
+    Parameters:
+    -----------------
+    Ks: array of curavture over multiple segments
+    win: integer, number of segments to be averaged over
+    """
+    K_avg = pd.DataFrame(Ks) #convert to df to perform the groupy function
+    Kt_avg=K_avg.T
+    Kt_avg=Kt_avg.groupby(Kt_avg.index // win).mean() #takes the average of 5 segments resulting in 20 rows
+    print('number of rows and columns:'+str(Kt_avg.shape))
+    Kt_avg=Kt_avg.to_numpy()
+    return (Kt_avg)
