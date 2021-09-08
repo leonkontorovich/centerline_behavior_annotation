5c931a9422b74eba660851b9bc7fe1cc4ada508d
Ulises Rey <ulises.rey@imp.ac.at>
2021-09-08 16:46:53 +0200

    added a wrapper function

--- centerline/dev/head_and_tail.py
+++ centerline/dev/head_and_tail.py
@@ -165,3 +165,66 @@ def assign_head_and_tail_to_coords(head_coords, tail_coords, candidate_coords):
         skel_head_coords=(np.nan, np.nan)
         skel_tail_coords=(np.nan, np.nan)
     return skel_head_coords, skel_tail_coords
+
+def head_and_tail_wrapper(hdf5_dlc_path, img_path, csv_output_filepath):
+    """
+    Wrapper of the head_and_tail functions
+    
+    Parameters:
+    -----------
+    hdf5_dlc_path, str
+    img_path, str
+    csv_output_filepath, str
+    Returns:
+    -----------
+    Nothing, writes a csv file in csv_output_filepath
+    """
+    #DLC coords
+    df=pd.read_hdf(hdf5_dlc_path)
+
+    head_coords=load_bodypart_coords_from_DLC(df, 'Head')
+    tail_coords=load_bodypart_coords_from_DLC(df, 'Tail')
+
+    #image
+    tif=tiff.TiffFile(img_path)
+    #csv object
+    csv_writer_path=open(csv_output_filepath,'w', newline='')
+
+    csv_writer_object=csv.writer(csv_writer_path)
+    csv_writer_object.writerow(('head coords x', 'head coords y','tail coords x', 'tail coords y'))
+
+    #for loop for each row in the dataframe of DLC coordinates (should be the same as number of frames)
+    for idx, row in df.iterrows():
+        #print(idx)
+        if idx%1000==0:
+            print(idx)#continue
+            elapsed = time.time() - t
+            print(elapsed)
+        #prepare image
+        img=tif.pages[idx].asarray()
+
+        skel=skeletonize(img/255)
+
+        pixel_graph, coordinates, degrees = skeleton_to_csgraph(skel)
+        #my function to get the edge_coords
+        number_of_neighbours=1
+        edge_coords=get_skeleton_points(skel, number_of_neighbours)
+
+        if edge_coords: #if edge_coords is not empty
+            #prepare head and tail coords
+            #assign head and tail coordinates to tuples
+            head_coords_i=(int(head_coords[1][idx]),int(head_coords[0][idx]))
+            tail_coords_i=(int(tail_coords[1][idx]),int(tail_coords[0][idx]))
+
+            skel_head,skel_tail=assign_head_and_tail_to_coords(head_coords_i, tail_coords_i, candidate_coords=edge_coords)
+            print(type(skel_head))
+
+            if np.isnan(skel_head[0]):
+                skel_head,skel_tail=head_coords_i, tail_coords_i
+
+        if not edge_coords:
+            skel_head,skel_tail=head_coords_i, tail_coords_i
+        print(type(skel_head))    
+        csv_writer_object.writerow(skel_head+skel_tail)
+
+    csv_writer_path.close()
