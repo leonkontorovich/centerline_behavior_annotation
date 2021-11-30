e4730fffd06f98ed5edbab49cc9b3d4985e29e40
Ulises Rey <ulises.rey@imp.ac.at>
2021-11-30 18:20:43 +0100

    Create create_global_PC.py

---
+++ dev/create_global_PC.py
@@ -0,0 +1,6 @@
+#This script should create a very long array containing all the curvatures from all the worms.
+# so that the PC are general and not specific to one worm.
+
+#Itamar has code to do this but it is on bitbucket (I think) and I cant access it now.
+
+#Whether BH annotation should be from here or from the PC of each worm is not clear as now.
