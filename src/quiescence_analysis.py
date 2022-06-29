78b71e78ab923b32883a77ca1208e86a531edede
Itamar-Lev <66638089+Itamar-Lev@users.noreply.github.com>
2022-06-29 23:30:39 +0200

    added function to calculate average worm size
    added function to calculate average worm size

--- src/quiescence_analysis.py
+++ src/quiescence_analysis.py
@@ -50,3 +50,50 @@ def get_frame_diff(img,frame_shift:int=3,contour_size_thresh:float=1.5):
 
     return frame_diff_arr
 
+
+from math import floor
+def get_average_worm_area(img, fraction_frames: float = 0.3, debug: bool = False):
+    """
+    recieves an binarize image stack, make sure it has at least 3 dimentions.
+    measures the average worm size (area in pixels),
+    to save processing time takes only fraction of all frames defined in fraction_frames
+    """
+
+    # make sure image is shape is usable
+    if len(img.shape) < 3:
+        if debug: print("image file currupt, less than 3 dimentions")
+        return None
+
+    # calculate which frames to take
+    total_frames = img.shape[2]
+    if fraction_frames < 0.01 or fraction_frames > 1:
+        fraction_frames = 0.3
+    frames_interval = floor(total_frames / (total_frames * fraction_frames))
+    frames_range = range(0, total_frames, frames_interval)
+    total_frames_taken = len(frames_range)
+    # initialize numpy array to hold area data
+    measured_area = np.empty(total_frames_taken)
+    measured_area[:] = np.nan
+
+    # loop over frames and get area
+    for idx, i in enumerate(frames_range):
+        curr_frame = img[:, :, idx]
+        # -get main segment
+        labeles = label(curr_frame)
+        segments = regionprops(labeles)
+        # make sure there's only one segment..
+        # Itamar we could implement take the biggest if there's more than one
+        if len(segments) == 1:
+            measured_area[idx] = segments[0].area
+        else:
+            # skip if there's not one clear object
+            continue
+
+    average_worm_area = np.nanmean(measured_area)
+
+    if np.isnan(average_worm_area):
+        if debug: print("problem with getting worm average size")
+        return None
+
+    return average_worm_area
+
