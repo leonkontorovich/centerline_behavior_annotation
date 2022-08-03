bc3e6c4dbdc4d65eeb7416735fb5ae94e968007d
Itamar-Lev <66638089+Itamar-Lev@users.noreply.github.com>
2022-08-03 19:56:26 +0200

    typo in path

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="quiescence",
+    name="quiescence_analysis",
     version="0.0.1",
     author="Itamar Lev and Johaness Frank",
     author_email="",
