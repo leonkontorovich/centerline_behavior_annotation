c0577769fa24b3f21515ebcbfb5a0509dd7da4f8
Ulises Rey <ulises.rey@imp.ac.at>
2022-08-09 15:40:41 +0200

    renamed test1.py

--- test/test1.yp.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-sys.path.append(str('/Volumes/scratch/neurobiology/zimmer/ulises/code/curvature_analysis/curvature_analysis-pkg'))
-print(sys.path)
-
-from dev.annotate_behaviour import *
-from src.make_PCA import *
