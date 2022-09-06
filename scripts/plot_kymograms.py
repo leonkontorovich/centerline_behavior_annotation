ba891c4f730208cc73071314d971b66c4615ab15
Ulises Rey <ulises.rey@imp.ac.at>
2022-09-06 12:16:18 +0200

    restructured the code so it has try and except. added a missing "/" in beh_paths end

--- scripts/plot_kymograms.py
+++ scripts/plot_kymograms.py
@@ -6,16 +6,20 @@ import os
 import glob
 
 
-main_path='/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20220729/20220729_12ms/data/'
+main_path='/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20220127/data/'
 
-beh_paths=glob.glob(os.path.join(main_path,'*worm*/*BH*'))
+beh_paths=glob.glob(os.path.join(main_path,'*worm*/*beh*/'))
 
 for project in beh_paths:
     print(project)
     kymo_path = os.path.join(project, 'skeleton_spline_K.csv')
-    df_kymo=pd.read_csv(kymo_path, header=None)
-    print(df_kymo.shape)
-    fig, axes=plt.subplots()#dpi=400, figsize=(40,4),)
-    fig.suptitle(kymo_path)
-    axes.imshow(df_kymo.T,origin="upper",cmap='seismic',extent=[0,df_kymo.shape[0],df_kymo.shape[1],0],aspect=20,vmin=-0.06,vmax=0.06)
-    plt.show()
+    try:
+        df_kymo=pd.read_csv(kymo_path, header=None)
+        print(df_kymo.shape)
+        fig, axes = plt.subplots()  # dpi=400, figsize=(40,4),)
+        fig.suptitle(kymo_path)
+        axes.imshow(df_kymo.T, origin="upper", cmap='seismic', extent=[0, df_kymo.shape[0], df_kymo.shape[1], 0],
+                    aspect=20, vmin=-0.06, vmax=0.06)
+        plt.show()
+    except: print('problem reading the kymograph csv file')
+
