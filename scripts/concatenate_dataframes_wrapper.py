edf7f8bc704fbd5225e549798a8c6db5f4796511
Ulises Rey <ulises.rey@univie.ac.at>
2022-10-11 16:42:20 +0200

    Revert "Improved it to work on Spline_K Spline_X and Spline_Y files"
    This reverts commit d84c4e2d1f29d0fac01292bac15965a82b82bf90.

--- scripts/concatenate_dataframes_wrapper.py
+++ scripts/concatenate_dataframes_wrapper.py
@@ -4,30 +4,13 @@ if __name__ == "__main__":
     import os
     from curvature.src.make_PCA import *
 
-    parser = argparse.ArgumentParser(description='Description of your program')
-    parser.add_argument('-i_K', '--input_spline_K', help='csv file with the spline curvature', required=True)
-    parser.add_argument('-i_X', '--input_spline_X', help='csv file with the spline X coords', required=True)
-    parser.add_argument('-i_Y', '--input_spline_Y', help='csv file with the spline Y coords', required=True)
-    parser.add_argument('-o', '--o_path', help='output path, has t be .csv file', required=True)
+    main_path='/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20220729/20220729_12ms/data/'
+    dataframe_path_list = glob.glob(os.path.join(main_path, '*worm*/*BH/*skeleton_spline_K.csv'))
 
-    args = vars(parser.parse_args())
-    spline_K = args['input_spline_K']
-    spline_X = args['input_spline_X']
-    spline_Y = args['input_spline_Y']
+    #print(dataframe_path_list)
 
-    o_path = args['o_path']
-
-    #main_path='/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20220729/20220729_12ms/data/'
-    #dataframe_path_list = glob.glob(os.path.join(main_path, '*_spline_K.csv'))
-
-    dataframe_path_list = [spline_K,
-    spline_X,
-    spline_Y]
-    print(dataframe_path_list)
-
-    # call the funcgtion
     concatenated_dataframe = concatenate_dataframes(dataframe_path_list)
 
-    #o_path = '/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20220729/20220729_12ms/analysis/merged_skeleton_spline_K.csv'
+    output_path = '/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20220729/20220729_12ms/analysis/merged_skeleton_spline_K.csv'
     #very important to have index and header to False!
-    concatenated_dataframe.to_csv(o_path, index=False, header=False)
+    concatenated_dataframe.to_csv(output_path, index=False, header=False)
