110e1faf5ab021d99a6a48790fd5049f7a9392fb
Ulises Rey <ulises.rey@univie.ac.at>
2022-10-11 16:45:55 +0200

    added warning about which code to use to reformat, see https://github.com/Zimmer-lab/curvature/commit/edf7f8bc704fbd5225e549798a8c6db5f4796511

--- scripts/concatenate_dataframes_wrapper.py
+++ scripts/concatenate_dataframes_wrapper.py
@@ -4,6 +4,12 @@ if __name__ == "__main__":
     import os
     from curvature.src.make_PCA import *
 
+    ###############
+    ## IMPORTANT!##
+    print("Do not use this code to reformat. To reformat the files there is another function: https://github.com/Zimmer-lab/centerline/blob/master/centerline/dev/reformat_skeleton_files.py")
+    ## IMPORTANT!##
+    ###############
+
     main_path='/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20220729/20220729_12ms/data/'
     dataframe_path_list = glob.glob(os.path.join(main_path, '*worm*/*BH/*skeleton_spline_K.csv'))
 
