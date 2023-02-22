28e47299d7fab0a14f62fa5977572e95f3923750
Ulises Rey <ulises.rey@univie.ac.at>
2023-02-22 18:14:15 +0100

    wrote a short code with SVM to classify turns, works kind of!

---
+++ behavior_analysis/src/ventral_dorsal_classifier.py
@@ -0,0 +1,100 @@
+# After this:
+# https://vitalflux.com/classification-model-svm-classifier-python-example/
+
+# Basic packages
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Sklearn modules & classes
+from sklearn.linear_model import Perceptron, LogisticRegression
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn import datasets
+from sklearn import metrics
+
+def classifier_example():
+    # Load the data set; In this example, the breast cancer dataset is loaded.
+
+    bc = datasets.load_breast_cancer()
+    X = bc.data
+    y = bc.target
+
+    # Create training and test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
+
+    sc = StandardScaler()
+    sc.fit(X_train)
+    X_train_std = sc.transform(X_train)
+    X_test_std = sc.transform(X_test)
+
+    # Instantiate the Support Vector Classifier (SVC)
+    svc = SVC(C=1.0, random_state=1, kernel='linear')
+
+    # Fit the model
+    svc.fit(X_train_std, y_train)
+
+    # Make the predictions
+    y_predict = svc.predict(X_test_std)
+
+    # Measure the performance
+    print("Accuracy score %.3f" % metrics.accuracy_score(y_test, y_predict))
+
+    return None
+
+# Load the data set; In this example, the breast cancer dataset is loaded.
+
+#load data
+spline_path = "/Users/ulises.rey/local_data/test_beh_annotation/skeleton_spline_K_signed_avg.csv"
+spline_df = pd.read_csv(spline_path, header=None)
+curvature = spline_df.iloc[:,30:80].sum(axis=1)
+curvature.fillna(0, inplace=True)
+curvature = pd.DataFrame(curvature, columns=['curvature'])
+
+pca_path = "/Users/ulises.rey/local_data/test_beh_annotation/principal_components.csv"
+pca_df = pd.read_csv(pca_path)
+pca_df.fillna(0, inplace=True)
+
+# concatenate pca_df and curvature, addining header 'curvature' to the curvature column
+
+data_df = pd.concat([pca_df, curvature], axis=1)
+
+X = data_df[['PC1','PC2','PC3','PC4','PC5','curvature']].values
+#load target
+target_path = "/Users/ulises.rey/local_data/test_beh_annotation/turn_annotation_timeseries.csv"
+y = pd.read_csv(target_path)['Annotation'].values
+
+#print where y is nan
+print(np.where(np.isnan(y)))
+
+print(X.shape, "is the shape of the data")
+print(y.shape, "is the shape of the target")
+# Create training and test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+# Instantiate the Support Vector Classifier (SVC)
+svc = SVC(C=1.0, random_state=1, kernel='poly')
+
+# Fit the model
+svc.fit(X_train_std, y_train)
+
+# Make the predictions
+y_predict = svc.predict(X_test_std)
+
+#compare y_test and y_predict with sklearn functions
+print("Accuracy score %.3f" % metrics.accuracy_score(y_test, y_predict))
+print(y_test)
+print(y_predict)
+
+#plot to see the results
+all_predict=svc.predict(sc.transform(X))
+plt.plot(all_predict)
+plt.plot(y, alpha=.5)
+
+print('end')
