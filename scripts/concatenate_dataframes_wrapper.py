9fdbc1b4af73b05d27103799e05080c041c722d0
Ulises Rey <ulises.rey@univie.ac.at>
2023-02-24 09:53:50 +0100

    improved description

--- scripts/concatenate_dataframes_wrapper.py
+++ scripts/concatenate_dataframes_wrapper.py
@@ -6,17 +6,17 @@ if __name__ == "__main__":
 
     ###############
     ## IMPORTANT!##
-    print("Do not use this code to reformat. To reformat the files there is another function: https://github.com/Zimmer-lab/centerline/blob/master/centerline/dev/reformat_skeleton_files.py")
+    print("Use this code to concatenate. Do not use this code to reformat. To reformat the files there is another function: https://github.com/Zimmer-lab/centerline/blob/master/centerline/dev/reformat_skeleton_files.py")
     ## IMPORTANT!##
     ###############
 
-    main_path='/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20220729/20220729_12ms/data/'
-    dataframe_path_list = glob.glob(os.path.join(main_path, '*worm*/*BH/*skeleton_spline_K.csv'))
+    main_path='/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/2022*/data/'
+    dataframe_path_list = glob.glob(os.path.join(main_path, '*worm*/*BH*/*skeleton_spline_K_signed.csv'))
 
-    #print(dataframe_path_list)
+    print(len(dataframe_path_list))
 
     concatenated_dataframe = concatenate_dataframes(dataframe_path_list)
 
-    output_path = '/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20220729/20220729_12ms/analysis/merged_skeleton_spline_K.csv'
+    output_path = '/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/merged_skeleton_spline_K_signed.csv'
     #very important to have index and header to False!
     concatenated_dataframe.to_csv(output_path, index=False, header=False)
