429e62f8f1428ebb3c0f5024ccff8a0c377784df
Ulises Rey <ulises.rey@univie.ac.at>
2023-06-01 15:02:55 +0200

    almost final version

--- behavior_analysis/src/self_touch.py
+++ behavior_analysis/src/self_touch.py
@@ -41,31 +41,23 @@ def find_specific_contours_with_specific_children(img, external_contour_area, in
 
     return specific_contours_with_specific_children
 
-def stack_self_touch(path, external_contour_area, internal_contour_area):
+def stack_self_touch(binary_path, external_contour_area, internal_contour_area):
     """
+    This function was written to detect self touch in worm, which will form a contour with children.
+    :param binary_path: path to the binary stack image
+    :param external_contour_area: area range of the external contour (min and max), e.g. [7000, 20000]
+    :param internal_contour_area: area range the internal contour (min and max), e.g. [100, 2000]
+    """
+    df = pd.DataFrame(columns=['self_touch'])
 
-    :param path:
-    :param external_cnt_area:
-    :param internal_contour_area:
-    :return:
-    """
-    with tiff.TiffFile(path) as tif_binary:
+    with tiff.TiffFile(binary_path) as tif_binary:
         for i, page in enumerate(tif_binary.pages):
             img = page.asarray()
             contours_with_children = find_specific_contours_with_specific_children(img, external_contour_area, internal_contour_area)
             if contours_with_children:
                 print('self touch')
-                df = df.append({'self_touch': 1}, ignore_index=True)
+                df.append({'self_touch': 1}, ignore_index=True)
             else:
                 print('no self touch')
-                df = df.append({'self_touch': 0}, ignore_index=True)
+                df.append({'self_touch': 0}, ignore_index=True)
     return df
-
-
-if __name__ == "__main__":
-
-path =
-
-df = stack_self_touch(path, [7000, 20000], [100, 2000])
-df.to_csv('/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20221127/data/ZIM2165_Gcamp7b_worm1/2022-11-27_15-14_ZIM2165_worm1_GC7b_Ch0-BH/self_touch.csv')
-print('done')
