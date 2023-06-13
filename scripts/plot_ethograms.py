5fd587c94f916e3e36bf36d7628c622da4cd97f3
Ulises Rey <ulises.rey@univie.ac.at>
2023-06-13 17:07:28 +0200

    improved fetch with glob.glob

--- scripts/plot_ethograms.py
+++ scripts/plot_ethograms.py
@@ -7,17 +7,17 @@ import glob
 from curvature.src.annotate_behaviour import ethogram_figure
 
 
-main_path='/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20220729/20220729_12ms/data/'
+main_path='/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20221013/data/'
 
 beh_paths=glob.glob(os.path.join(main_path,'*worm*/*BH*'))
 
 for project in beh_paths:
     print(project)
 
-    kymo_path=os.path.join(project, 'skeleton_spline_K.csv')
+    kymo_path=glob.glob(os.path.join(project, '*skeleton_spline_K.csv'))[0]
     df_kymo=pd.read_csv(kymo_path, header=None)
 
-    beh_annotation_path = os.path.join(project, 'beh_annotation.csv')
+    beh_annotation_path = glob.glob(os.path.join(project, '*beh_annotation.csv'))[0]
     beh_annotation_df = pd.read_csv(beh_annotation_path, index_col=0, header=None)
     # beh_annotation_df= beh_annotation_df*-1
     # beh_annotation_df.to_csv(os.path.join(project, 'beh_annotation.csv'))
