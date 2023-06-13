c6bb1ee30143b04dc6a0a5feeff5e8f11b1d095c
Ulises Rey <ulises.rey@univie.ac.at>
2023-06-13 17:06:34 +0200

    added parser

--- scripts/concatenate_dataframes_wrapper.py
+++ scripts/concatenate_dataframes_wrapper.py
@@ -10,10 +10,19 @@ if __name__ == "__main__":
     ## IMPORTANT!##
     ###############
 
-    main_path='/scratch/neurobiology/zimmer/ulises/wbfm/2022*/data/'
-    dataframe_path_list = glob.glob(os.path.join(main_path, '*worm*/*BH*/*skeleton_spline_K_signed_avg.csv'))
+    #specify list in bash
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataframe_path_list', nargs='+', help='list of paths to dataframes to concatenate')
+
+    args = parser.parse_args()
+    dataframe_path_list = args.dataframe_path_list
+
+    #specify list in python
+    # main_path='/scratch/neurobiology/zimmer/ulises/wbfm/2022*/data/'
+    # dataframe_path_list = glob.glob(os.path.join(main_path, '*worm*/*BH*/*skeleton_spline_K_signed_avg.csv'))
 
     print("There are ", len(dataframe_path_list), "spline_K_files to concatenate")
+    print("These are the files that you will concatenate: ", dataframe_path_list)
 
     concatenated_dataframe = concatenate_dataframes(dataframe_path_list)
 
