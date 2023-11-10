5c12891e80bf968531cc4268441480358148c176
BenjaminSchaar <122351773+BenjaminSchaar@users.noreply.github.com>
2023-11-10 11:44:54 +0100

    added changes for downsample step

--- pipeline_cluster/autoscope_pipeline/RUNME_cluster.sh
+++ pipeline_cluster/autoscope_pipeline/RUNME_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Example from: https://hackmd.io/@bluegenes/BJPrrj7WB
-OPT="sbatch --time {cluster.time} -p {cluster.partition} --cpus-per-task {cluster.cpus_per_task} --mem {cluster.mem} --output {cluster.output}"
+OPT="sbatch -p {cluster.partition} --cpus-per-task {cluster.cpus_per_task} --mem {cluster.mem} --output {cluster.output}"
 #NUM_JOBS_TO_SUBMIT=2
 # Untested : Modify if necessary
 NUM_JOBS_TO_SUBMIT=$(find $PWD -mindepth 3 -maxdepth 3 -type d -wholename "*/*/*w*/*Ch0" | wc -l)
@@ -9,3 +9,6 @@ echo "Submitting $NUM_JOBS_TO_SUBMIT Jobs. Make sure that these are the number o
 # As of 8/2022 your home folder at /home/user should be writable from the cluster, but this may be temporary
 # export HOME="/scratch/neurobiology/zimmer/YOUR/USER"
 snakemake --configfile config.yaml --latency-wait 60 --use-conda --cluster "$OPT" --cluster-config cluster_config.yaml --jobs $NUM_JOBS_TO_SUBMIT
+
+#unlock snakemake folders
+#snakemake --configfile config.yaml --latency-wait 60 --use-conda --cluster "$OPT" --cluster-config cluster_config.yaml --jobs $NUM_JOBS_TO_SUBMIT --unlock
