51dca03544b4082ad89f985d3265fb5a08604ae8
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2024-01-23 09:38:50 +0100

    Consolidate setup.py from the subprojects

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="curvature",
-    version="0.0.2",
+    name="centerline_behavior_annotation",
+    version="0.0.3",
     author="Ulises Rey and Itamar Lev",
     author_email="ulises.rey@univie.ac.at",
-    description="A small package to analyze worm curvature",
+    description="A package to analyze worm curvature",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="",
