49cfe9dc80f17b950532051e4b84fcacef445498
BenjaminSchaar <122351773+BenjaminSchaar@users.noreply.github.com>
2024-07-26 14:36:52 +0200

    added newest version of working pipelin with unet

--- pipeline_cluster/centerline_pipeline/OAS1/unet_based_segmentation/RUNME_cluster.sh
+++ pipeline_cluster/centerline_pipeline/OAS1/unet_based_segmentation/RUNME_cluster.sh
@@ -7,7 +7,8 @@ NUM_JOBS_TO_SUBMIT=$(find "$PWD" -mindepth 3 -maxdepth 3 -type d -wholename "*/*
 echo "Submitting $NUM_JOBS_TO_SUBMIT Jobs. Make sure that these are the number of datasets otherwise expect errors."
 
 # Use snakemake with specified options
-snakemake --configfile config.yaml --latency-wait 500 --cluster "$OPT" --cluster-config cluster_config.yaml --jobs $NUM_JOBS_TO_SUBMIT --rerun-incomplete
-#snakemake --configfile config.yaml --latency-wait 120 --use-conda --cores 1
+snakemake --configfile config.yaml --latency-wait 500 --cluster "$OPT" --cluster-config cluster_config.yaml --jobs $NUM_JOBS_TO_SUBMIT --rerun-incomplete --forcerun create_plots2
+#snakemake --configfile config.yaml --latency-wait 120 --use-conda --cores 1 --rerun-incomplete
 #snakemake --configfile config.yaml --latency-wait 120 --use-conda --cores 1 --unlock
-# snakemake --configfile config.yaml --latency-wait 120 --cores 1
+#snakemake --configfile config.yaml --latency-wait 120 --cores 1
+#snakemake --configfile config.yaml --latency-wait 120 --cores 1 --delete-all-output --rerun-incomplete
