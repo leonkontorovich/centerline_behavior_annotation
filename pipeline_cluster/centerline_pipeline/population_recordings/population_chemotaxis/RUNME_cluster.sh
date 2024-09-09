b65c85895df00a9aaec50e5bc69ac4e5641df0fd
BenjaminSchaar <122351773+BenjaminSchaar@users.noreply.github.com>
2024-09-09 13:11:10 +0200

    updated pipelin files for population chemotaxis

--- pipeline_cluster/centerline_pipeline/population_recordings/population_chemotaxis/RUNME_cluster.sh
+++ pipeline_cluster/centerline_pipeline/population_recordings/population_chemotaxis/RUNME_cluster.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Example from: https://hackmd.io/@bluegenes/BJPrrj7WB
-OPT="sbatch -p {cluster.partition} -t {cluster.time} --cpus-per-task {cluster.cpus_per_task} --mem {cluster.mem} --output {cluster.output}"
-#NUM_JOBS_TO_SUBMIT=2
-# Untested : Modify if necessary
-NUM_JOBS_TO_SUBMIT=$(find $PWD -mindepth 3 -maxdepth 3 -type d -wholename "*/*/*w*/*Ch0" | wc -l)
+OPT="sbatch -t {cluster.time} -p {cluster.partition} --cpus-per-task {cluster.cpus_per_task} --mem {cluster.mem} --output {cluster.output} --gres {cluster.gres}"
+
+# Count the number of directories three levels deep that match the specific pattern and end in 'Ch0'
+NUM_JOBS_TO_SUBMIT=$(find "$PWD" -mindepth 3 -maxdepth 3 -type d -wholename "*/*track*/" | wc -l)
 echo "Submitting $NUM_JOBS_TO_SUBMIT Jobs. Make sure that these are the number of datasets otherwise expect errors."
-# Needs writable cache
-# As of 8/2022 your home folder at /home/user should be writable from the cluster, but this may be temporary
-# export HOME="/scratch/neurobiology/zimmer/YOUR/USER"
-snakemake --configfile config.yaml --latency-wait 240 --use-conda --cluster "$OPT" --cluster-config cluster_config.yaml --jobs $NUM_JOBS_TO_SUBMIT --rerun-incomplete
-#snakemake --configfile config.yaml --latency-wait 240 --use-conda --cores 1
+
+# Use snakemake with specified options
+#snakemake --configfile config.yaml --latency-wait 500 --cluster "$OPT" --cluster-config cluster_config.yaml --jobs $NUM_JOBS_TO_SUBMIT --rerun-incomplete
+snakemake --configfile config.yaml --latency-wait 120 --use-conda --cores 1 --forceall
+
+#snakemake --configfile config.yaml --latency-wait 120 --use-conda --cores 1 --unlock
+#snakemake --configfile config.yaml --latency-wait 120 --use-conda --cores 1 --delete-all-output
+# snakemake --configfile config.yaml --latency-wait 120 --cores 1
